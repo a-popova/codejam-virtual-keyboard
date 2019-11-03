d393337c9f5dd08c5a40790ec9d257a4e1978b52
a-popova <papova.anastasiya.13@gmail.com>
2019-11-03 17:32:37 +0300

    feat: implement function type() for physical keyboard

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -9,33 +9,61 @@ window.onload = function() {
   this.document.body.innerHTML += keyboard_rus;
 
   this.document.body.addEventListener("click", click);
-  this.document.body.addEventListener("keydown", function(event){highlight(document.getElementById(event.code)), type(document.getElementById(event.code))});
-}
-//type(document.getElementById(event.code))
-function click (){
-  let target = event.target;
-  highlight(target);
-  type(target);
-  target = target.parentNode;
-  
+  this.document.body.addEventListener("keydown", function(event){highlight(document.getElementById(event.code)), type(document.getElementById(event.code)), remove(document.getElementById(event.code))});
+  this.document.body.addEventListener("keyup", function(event){highlightOff(document.getElementById(event.code))});
 }
 
 function highlight(node) {
    if (node.tagName === 'SPAN' || node.tagName === 'SUP'){
      node = node.parentElement;
    } 
-   node.classList.add("active")
-   let t = setTimeout(function(){
-    node.classList.remove("active");
-   },(200));   
+   node.classList.add("active");
+   result.focus();
 }
 
-function type(char){
-  let result = document.body.querySelector("#result");
+function highlightOff(node){
+  if (node.tagName === 'SPAN' || node.tagName === 'SUP'){
+    node = node.parentElement;
+  }
+  node.classList.remove("active");
+  result.focus();
+}
+
+function typeVirtual(char){
+  var result = document.body.querySelector("#result");
+  result.setAttribute("readonly", true);
   if (char.tagName === 'DIV'){
     char = char.firstElementChild;
   } else if (char.tagName === 'SUP'){
     char = char.previousElementSibling;
-  } 
+  } else if (char.textContent === "Backspace"){
+    remove(char.textContent);
+  }
   result.value += char.textContent;
+  result.focus();
+}
+
+function type(char){
+  result.setAttribute("readonly", true);
+  if (char.tagName === 'DIV'){
+    char = char.firstElementChild;
+  } else if (char.tagName === 'SUP'){
+    char = char.previousElementSibling;
+  }
+  result.value += char.textContent;
+  result.focus();
+}
+
+function remove(char) {
+  if (char === "Backspace"){
+    result.value.substring(0, result.value.length-1);
+  }
+}
+
+function click (){
+  let target = event.target;
+  highlight(target);
+  typeVirtual(target);
+  target = target.parentNode;
+  result.focus();
 }
