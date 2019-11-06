34f8d6eb546286b503cee81b0acbd6f00cb0c6b4
a-popova <papova.anastasiya.13@gmail.com>
2019-11-06 13:49:59 +0300

    Initial commit

---
+++ src/javascript/input.js
@@ -0,0 +1,2 @@
+const input = '<textarea id="result" rows=6 cols=60 autofocus>';
+export default input;
---
+++ src/javascript/keyboard_rus.js
@@ -0,0 +1,83 @@
+const keyboardRus = `<div class="keyboard rus">
+        <h4>Change language by clicking <span class="key">Shift</span> + <span class="key">Ctrl</span></h4>
+      
+        <div class="row">
+          <div class="key" id="rus-Backquote"><span>ё</span><sup>~</sup></div>
+          <div class="key" id="rus-Digit1"><span>1</span><sup>!</sup></div>
+          <div class="key" id="rus-Digit2"><span>2</span><sup>"</sup></div>
+          <div class="key" id="rus-Digit3"><span>3</span><sup>№</sup></div>
+          <div class="key" id="rus-Digit4"><span>4</span><sup>;</sup></div>
+          <div class="key" id="rus-Digit5"><span>5</span><sup>%</sup></div>
+          <div class="key" id="rus-Digit6"><span>6</span><sup>:</sup></div>
+          <div class="key" id="rus-Digit7"><span>7</span><sup>?</sup></div>
+          <div class="key" id="rus-Digit8"><span>8</span><sup>*</sup></div>
+          <div class="key" id="rus-Digit9"><span>9</span><sup>(</sup></div>
+          <div class="key" id="rus-Digit0"><span>0</span><sup>)</sup></div>
+          <div class="key" id="rus-Minus"><span>-</span><sup>_</sup></div>
+          <div class="key" id="rus-Equal"><span>=</span><sup>+</sup></div>
+          <div class="key backspace" id="rus-Backspace"><span>Backspace</span></div>
+        </div>
+        
+        <div class="row">
+          <div class="key tab" id="rus-Tab"><span>Tab</span></div>
+          <div class="key" id="rus-KeyQ"><span>й</span></div>
+          <div class="key" id="rus-KeyW"><span>ц</span></div>
+          <div class="key" id="rus-KeyE"><span>у</span></div>
+          <div class="key" id="rus-KeyR"><span>к</span></div>
+          <div class="key" id="rus-KeyT"><span>е</span></div>
+          <div class="key" id="rus-KeyY"><span>н</span></div>
+          <div class="key" id="rus-KeyU"><span>г</span></div>
+          <div class="key" id="rus-KeyI"><span>ш</span></div>
+          <div class="key" id="rus-KeyO"><span>щ</span></div>
+          <div class="key" id="rus-KeyP"><span>з</span></div>
+          <div class="key" id="rus-BracketLeft"><span>х</span></div>
+          <div class="key" id="rus-BracketRight"><span>ъ</span></div>
+          <div class="key" id="rus-Backslash"><span>\\</span><sup>/</sup></div>
+        </div>
+        
+        <div class="row">
+          <div class="key capslock" id="rus-CapsLock"><span>Caps Lock</span></div>
+          <div class="key" id="rus-KeyA"><span>ф</span></div>
+          <div class="key" id="rus-KeyS"><span>ы</span></div>
+          <div class="key" id="rus-KeyD"><span>в</span></div>
+          <div class="key" id="rus-KeyF"><span>а</span></div>
+          <div class="key" id="rus-KeyG"><span>п</span></div>
+          <div class="key" id="rus-KeyH"><span>р</span></div>
+          <div class="key" id="rus-KeyJ"><span>о</span></div>
+          <div class="key" id="rus-KeyK"><span>л</span></div>
+          <div class="key" id="rus-KeyL"><span>д</span></div>
+          <div class="key" id="rus-Semicolon"><span>ж</span></div>
+          <div class="key" id="rus-Quote"><span>э</span></div>
+          <div class="key enter" id="rus-Enter"><span>Enter</span></div>
+        </div>
+        
+        <div class="row">
+          <div class="key shift" id="rus-ShiftLeft"><span>Shift</span></div>
+          <div class="key" id="rus-KeyZ"><span>я</span></div>
+          <div class="key" id="rus-KeyX"><span>ч</span></div>
+          <div class="key" id="rus-KeyC"><span>с</span></div>
+          <div class="key" id="rus-KeyV"><span>м</span></div>
+          <div class="key" id="rus-KeyB"><span>и</span></div>
+          <div class="key" id="rus-KeyN"><span>т</span></div>
+          <div class="key" id="rus-KeyM"><span>ь</span></div>
+          <div class="key" id="rus-Comma"><span>б</span></div>
+          <div class="key" id="rus-Period"><span>ю</span></div>
+          <div class="key" id="rus-Slash"><span>.</span><sup>,</sup></div>
+          <div class="key up" id="rus-ArrowUp"><span>↑</span></div>
+          <div class="key shift" id="rus-ShiftRight"><span>Shift</span></div>
+        </div>
+        
+        <div class="row">
+          <div class="key ctrl" id="rus-ControlLeft"><span>ctrl</span></div>
+          <div class="key win" id="rus-MetaLeft"><span>win</span></div>
+          <div class="key alt" id="rus-AltLeft"><span>alt</span></div>
+          <div class="key space" id="rus-Space"></div>
+          <div class="key alt" id="rus-AltRight"><span>alt</span></div>
+          <div class="key ctrl" id="rus-ControlRight"><span>ctrl</span></div>
+          <div class="key left" id="rus-ArrowLeft"><span>←</span></div>
+          <div class="key down" id="rus-ArrowDown"><span>↓</span></div>
+          <div class="key right" id="rus-ArrowRight"><span>→</span></div>
+        </div>
+        
+      </div>`;
+export default keyboardRus;
